3e04a9438eaeffd0620d0cf19c157ebdff1dda3d
Wildan <fitrahjie995@gmail.com>
2024-12-09 21:20:44 +0800

    edit page register

--- pages/register.vue
+++ pages/register.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center items-center h-screen bg-gray-100 p-4">
     <div class="flex flex-col justify-center items-center bg-white p-8 rounded-lg shadow-md max-w-md w-full min-h-[400px] text-center">
-      <h1 class="mb-6 text-2xl font-bold text-gray-800">Register</h1>
+      <h1 class="mb-6 text-2xl font-bold text-gray-800">REGISTRASI</h1>
       <form @submit.prevent="register" class="flex flex-col gap-4 w-full max-w-xs">
         <div class="flex flex-col items-start w-full">
           <label for="name" class="text-sm text-gray-600 mb-2">Nama:</label>
@@ -47,7 +47,7 @@
           type="submit"
           class="w-full max-w-sm px-4 py-2 bg-green-500 text-white rounded transition duration-300 hover:bg-green-600 transform hover:scale-105"
         >
-          Register
+          Daftar
         </button>
       </form>
       <p v-if="error" class="mt-4 text-red-500 text-sm">{{ error }}</p>
