b09228f676016a5d68a50c7055b548bd749399d9
Wildan <fitrahjie995@gmail.com>
2024-12-09 21:24:02 +0800

    edit tailwind dan readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
